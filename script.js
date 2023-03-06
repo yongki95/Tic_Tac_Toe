734136e26d48262211a0e60322d3d2dfb4e18872
yongki95 <yongki95@vt.edu>
2023-03-06 11:22:36 -0800

    Add for loops to create grid cell and add class name

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 //gameboard array in gameboard object
 //make global code as little as possible
 
-//object create
-const Gameboard = (name = 'player', mark) => {
-    gbarray = [];
-    playerName = name;
-    mark = mark;    
-    //control the flow of game
+//object create 
+//gameBoard, displaycontroller(need only once) with module
+//player object with factory function
+const Gameboard = (() => {
+    const GbArray = new Array(9);
 
+    return {GbArray};
+})();
+
+
+//Create Grid Cell
+const gameBoardDiv = document.getElementById('gameboard');
+
+for(let i = 0; i < Gameboard.GbArray.length; i++){
+    const grid = document.createElement('div')
+    grid.className = 'grid';
+    gameBoardDiv.appendChild(grid); 
+    if(i%3 === 1) {
+        grid.classList.add('colCenter');
+    } 
+    if(i === 3 || i === 4 || i === 5){
+        grid.classList.add('rowCenter');
+    }
 }
+
+
+
+
+

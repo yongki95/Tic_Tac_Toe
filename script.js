da734036c8199f3af05bc2983fa8ef418d006cf3
yongki95 <yongki95@vt.edu>
2023-03-06 09:55:42 -0800

    Start write factory function about gameboard

---
+++ script.js
@@ -0,0 +1,11 @@
+//gameboard array in gameboard object
+//make global code as little as possible
+
+//object create
+const Gameboard = (name = 'player', mark) => {
+    gbarray = [];
+    playerName = name;
+    mark = mark;    
+    //control the flow of game
+
+}
